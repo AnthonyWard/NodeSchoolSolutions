87d774214281e697a0273bfc0185f95310049cdb
Anthony Ward <anthony.ward@centrica.com>
2015-01-14 15:11:09 +0000

    Added Solution to 10

--- learnyounode/10 time server.js
+++ learnyounode/10 time server.js
@@ -3,4 +3,10 @@
  */
 
 var net = require('net');
+var strftime = require('strftime');
 
+var server = net.createServer(function (socket) {
+    socket.end(strftime('%F %R', new Date()));
+});
+
+server.listen(process.argv[2]);
